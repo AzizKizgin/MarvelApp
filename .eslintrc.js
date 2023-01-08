f0493187467d17f2d2e08c75a0477f1d02e79668
Aziz Kizgin <azizkzgn@gmail.com>
2023-01-08 14:44:32 +0300

    feat: file updated

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,8 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
   rules: {
     'no-unused-vars': 'off',
     '@typescript-eslint/no-unused-vars': ['warn'],
