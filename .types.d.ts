8ef91822d7332e8416d0365a1646705b71ac8768
Aziz Kizgin <azizkzgn@gmail.com>
2023-01-15 17:51:52 +0300

    feat: new types added

--- .types.d.ts
+++ .types.d.ts
@@ -38,3 +38,154 @@ export type ComicParams = {
   limit?: number;
   offset?: number;
 };
+
+export type Character = {
+  id: number;
+  name: string;
+  description: string;
+  modified: string;
+  thumbnail: {
+    path: string;
+    extension: string;
+  };
+  resourceURI: string;
+  comics: {
+    available: number;
+    collectionURI: string;
+    items: {
+      resourceURI: string;
+      name: string;
+    }[];
+    returned: number;
+  };
+  series: {
+    available: number;
+    collectionURI: string;
+    items: {
+      resourceURI: string;
+      name: string;
+    }[];
+    returned: number;
+  };
+  stories: {
+    available: number;
+    collectionURI: string;
+    items: {
+      resourceURI: string;
+      name: string;
+      type: string;
+    }[];
+    returned: number;
+  };
+  events: {
+    available: number;
+    collectionURI: string;
+    items: {
+      resourceURI: string;
+      name: string;
+    }[];
+    returned: number;
+  };
+  urls: {
+    type: string;
+    url: string;
+  }[];
+};
+
+export type Comic = {
+  id: number;
+  digitalId: number;
+  title: string;
+  issueNumber: number;
+  variantDescription: string;
+  description: string;
+  modified: string;
+  isbn: string;
+  upc: string;
+  diamondCode: string;
+  ean: string;
+  issn: string;
+  format: string;
+  pageCount: number;
+  textObjects: {
+    type: string;
+    language: string;
+    text: string;
+  }[];
+  resourceURI: string;
+  urls: {
+    type: string;
+    url: string;
+  }[];
+  series: {
+    resourceURI: string;
+    name: string;
+  };
+  variants: {
+    resourceURI: string;
+    name: string;
+  }[];
+  collections: {
+    resourceURI: string;
+    name: string;
+  }[];
+  collectedIssues: {
+    resourceURI: string;
+    name: string;
+  }[];
+  dates: {
+    type: string;
+    date: string;
+  }[];
+  prices: {
+    type: string;
+    price: number;
+  }[];
+  thumbnail: {
+    path: string;
+    extension: string;
+  };
+  images: {
+    path: string;
+    extension: string;
+  }[];
+  creators: {
+    available: number;
+    collectionURI: string;
+    items: {
+      resourceURI: string;
+      name: string;
+      role: string;
+    }[];
+    returned: number;
+  };
+  characters: {
+    available: number;
+    collectionURI: string;
+    items: {
+      resourceURI: string;
+      name: string;
+      role: string;
+    }[];
+    returned: number;
+  };
+  stories: {
+    available: number;
+    collectionURI: string;
+    items: {
+      resourceURI: string;
+      name: string;
+      type: string;
+    }[];
+    returned: number;
+  };
+  events: {
+    available: number;
+    collectionURI: string;
+    items: {
+      resourceURI: string;
+      name: string;
+    }[];
+    returned: number;
+  };
+};
