34b36592abada61f4aac6cea2f70ee454c11d1c4
Aziz Kizgin <azizkzgn@gmail.com>
2023-01-15 21:48:52 +0300

    feat: getAllCharacters function is updated

--- src/data/queries.ts
+++ src/data/queries.ts
@@ -1,23 +1,50 @@
-import {useQuery} from '@tanstack/react-query';
+import {useInfiniteQuery, useQuery} from '@tanstack/react-query';
 import {Character, CharacterParams, Comic, ComicParams} from '../../.types';
 import {getCharacter, getCharacters, getComic, getComics} from '../utils/api';
 
 export const getAllCharacters = (params: CharacterParams) => {
-  const {data, isLoading, error, isFetching, isSuccess} = useQuery<{
-    data: {
-      results: Character[];
-    };
-  }>(['characters', params], () => getCharacters(params), {
-    enabled: !!params,
-  });
+  const {
+    fetchNextPage,
+    fetchPreviousPage,
+    hasNextPage,
+    hasPreviousPage,
+    isFetchingNextPage,
+    isFetchingPreviousPage,
+    data,
+    isLoading,
+    isFetching,
+    refetch,
+  } = useInfiniteQuery(
+    ['characters', params],
+    ({pageParam = 0}) => getCharacters({...params, offset: pageParam}),
+    {
+      getNextPageParam: (lastPage) => {
+        if (lastPage.data.total > lastPage.data.offset + lastPage.data.count) {
+          return lastPage.data.offset + lastPage.data.count;
+        }
+        return undefined;
+      },
+      getPreviousPageParam: (firstPage) => {
+        if (firstPage.data.offset > 0) {
+          return firstPage.data.offset - firstPage.data.count;
+        }
+        return undefined;
+      },
+    },
+  );
   return {
-    characters: data?.data?.results,
+    characters: data?.pages.map((page) => page.data.results).flat(),
     isLoading: isLoading && isFetching,
-    error,
-    isSuccess,
+    fetchNextPage,
+    fetchPreviousPage,
+    hasNextPage,
+    hasPreviousPage,
+    isFetchingNextPage,
+    isFetchingPreviousPage,
+    refetch,
+    isFetching,
   };
 };
-
 export const getCharacterById = (id: number) => {
   const {data, isLoading, error, isFetching, isSuccess} = useQuery<{
     data: {
