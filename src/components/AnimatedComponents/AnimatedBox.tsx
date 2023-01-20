65e3438c4a2bfa128b24152b717169103d5b33ad
Aziz Kizgin <azizkzgn@gmail.com>
2023-01-20 09:04:46 +0300

    feat: new animated components are added

---
+++ src/components/AnimatedComponents/AnimatedBox.tsx
@@ -0,0 +1,4 @@
+import {Box} from 'native-base';
+import Animated from 'react-native-reanimated';
+const AnimatedBox = Animated.createAnimatedComponent(Box);
+export default AnimatedBox;
---
+++ src/components/AnimatedComponents/AnimatedPressable.tsx
@@ -0,0 +1,5 @@
+import {Pressable} from 'native-base';
+import Animated from 'react-native-reanimated';
+
+const AnimatedPressable = Animated.createAnimatedComponent(Pressable);
+export default AnimatedPressable;
