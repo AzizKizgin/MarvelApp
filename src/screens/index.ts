0bbf796e5801cf0de1dace7c10a20d6fa0a1a217
Aziz Kizgin <azizkzgn@gmail.com>
2023-01-21 14:33:11 +0300

    feat: screens index file added

---
+++ src/screens/index.ts
@@ -0,0 +1,4 @@
+export {default as Feed} from './Feed';
+export {default as Favourites} from './Favourites';
+export {default as Account} from './Account';
+export {default as Details} from './Details';
