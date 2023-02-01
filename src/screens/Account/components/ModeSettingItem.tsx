bde8d7cd1295aa7576d4448f6a82101df234933c
Aziz Kizgin <azizkzgn@gmail.com>
2023-02-01 19:03:18 +0300

    feat: dark mode button added

--- src/screens/Account/components/ModeSettingItem.tsx
+++ src/screens/Account/components/ModeSettingItem.tsx
@@ -1,27 +1,25 @@
-import {HStack, Icon, Switch, Text} from 'native-base';
+import {HStack, Icon, Switch, Text, useColorMode} from 'native-base';
 import React, {useState} from 'react';
 import Ionicons from 'react-native-vector-icons/Ionicons';
+import theme from '../../../../Theme';
 const ModeSettingItem = () => {
-  const [isDarkMode, setIsDarkMode] = useState(false);
-
+  const {toggleColorMode, colorMode} = useColorMode();
   return (
     <HStack justifyContent={'space-between'} marginTop={4}>
-      {isDarkMode ? (
-        <HStack space={2} justifyContent={'center'} alignItems={'center'}>
-          <Icon as={<Ionicons name={'ios-moon'} />} color={'#405b67'} />
-          <Text fontSize={'lg'} color={'#405b67'} fontWeight={'medium'}>
-            Dark Mode
-          </Text>
-        </HStack>
-      ) : (
-        <HStack space={2} justifyContent={'center'} alignItems={'center'}>
-          <Icon as={<Ionicons name={'ios-sunny'} />} color={'#405b67'} />
-          <Text fontSize={'lg'} color={'#405b67'} fontWeight={'medium'}>
-            Light Mode
-          </Text>
-        </HStack>
-      )}
-      <Switch onChange={() => setIsDarkMode(!isDarkMode)} />
+      <HStack space={2} justifyContent={'center'} alignItems={'center'}>
+        <Icon as={<Ionicons name={'ios-moon'} />} color={'mainDarkBlue'} />
+        <Text fontSize={'lg'} color={'mainDarkBlue'} fontWeight={'medium'}>
+          Dark Mode
+        </Text>
+      </HStack>
+      <Switch
+        onChange={() => toggleColorMode()}
+        trackColor={{true: '#767577', false: theme.colors.mainLightBlue}}
+        thumbColor={
+          colorMode === 'dark' ? '#2f2932' : theme.colors.mainDarkBlue
+        }
+        isChecked={colorMode === 'dark'}
+      />
     </HStack>
   );
 };
